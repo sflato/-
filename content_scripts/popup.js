f14314ead39aa615623217b749bc0e4b75b2ca84
sflato <stefancvitanic@gmail.com>
2018-11-16 15:30:08 -0600

    added comments to popup script, fixed example script

--- content_scripts/popup.js
+++ content_scripts/popup.js
@@ -1,10 +1,9 @@
 
-  function openWindow (url, i){
-    let x =  Math.floor((Math.random() * window.screen.width) + 1)
-    let y =  Math.floor((Math.random() * window.screen.height) + 1)
-    let name = `window${i}`
-    window.open (url, name,`menubar=no,status=no,location=no,width=40,height=40,toolbar=no,scrollbars=no,top=${y},left=${x}`);
-  }
+  /**
+    * openAllWindows() selects every <a> tag on the webpage
+    * links.length can be changed to modify the amount
+    of popup windows that are opened
+  */
 
   function openAllWindows() {
     let links = document.body.querySelectorAll("a")
@@ -15,8 +14,19 @@
     }
   }
   /**
+    *places each popup window in a random location
+    inside the bounds of the screen size
+  */
+  function openWindow (url, i){
+    let x =  Math.floor((Math.random() * window.screen.width) + 1)
+    let y =  Math.floor((Math.random() * window.screen.height) + 1)
+    //gives each window a new name
+    let name = `window${i}`
+    window.open (url, name,`menubar=no,status=no,location=no,width=40,height=40,toolbar=no,scrollbars=no,top=${y},left=${x}`);
+  }
+  /**
    * Listen for messages from the background script.
-   * Call "beastify()" or "reset()".
+   * Call "popup()"
   */
   browser.runtime.onMessage.addListener((message) => {
     if (message.command === "popup") {
--- popup/onoffswitch.js
+++ popup/onoffswitch.js
@@ -1,14 +1,11 @@
 function listenForClicks() {
-  document.addEventListener("click", (e) => {
+   browser.tabs.sendMessage(tabs[0].id, {
+     command: "popup",
+   })
+  console.log("running");
+}
 
-    function popup(tabs) {
-        browser.tabs.sendMessage(tabs[0].id, {
-          command: "popup",
-        });
-      });
-    }
 
-  });
-browser.tabs.executeScript({file: "/content_scripts/popup.js"})
-.then(listenForClicks)
-.catch(reportExecuteScriptError);
+ browser.tabs.executeScript({file: "/content_scripts/popup.js"})
+ .then(listenForClicks)
+console.log("Hello world");
