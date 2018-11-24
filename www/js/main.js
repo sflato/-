dff12b2bab67875da13ec08cb26de904bf79536f
sflato <stefancvitanic@gmail.com>
2018-11-24 00:57:28 -0600

    experiments with window.open inside webpage

--- www/js/main.js
+++ www/js/main.js
@@ -1,29 +1,5 @@
+window.open ("1.html", "newWindow1","menubar=no,status=no,location=no,width=432,height=25,toolbar=no,scrollbars=no,top= 188,left= 439");
 
-window.onload = openAllWindows;
-
-function openAllWindows() {
-  let links = document.body.querySelectorAll("a")
-  for (let i = 0; i < 10; i++) {
-    let href = links[i].href
-    openWindow(href,i)
-
-  }
-}
-
-  //places each popup window in a random location
-  //inside the bounds of the screen size
-
-function openWindow (url, i){
-  let x =  Math.floor((Math.random() * window.screen.width) + 1)
-  let y =  Math.floor((Math.random() * window.screen.height) + 1)
-  let width = Math.floor((Math.random() * 500) + 1)
-  let height = Math.floor((Math.random() * 500) + 1)
-  //gives each window a new name
-  let name = `window${i}`
-  // --> `` <-- required to pass numbers into method
-  window.open (url, name,`menubar=no,status=no,location=no,
-    width=${width},height=${height},toolbar=no,scrollbars=no,
-    top=${y},left=${x}`);
 }
 
 
