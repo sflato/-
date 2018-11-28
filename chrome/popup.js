7ca6514eb6de5868b8ad223e1018aa5e22da014f
sflato <stefancvitanic@gmail.com>
2018-11-28 15:55:04 -0600

    introwindow() still not functional

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,4 +1,31 @@
+/** THIS IS THE SCRIPT THAT IS INJECTED
+INSIDE OF THE WEBPAGE YOU PRESS "START"
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+          // PROTEKNOVANDAL //
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+HAPPY SEARCHING (''/)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
+
+/**
+* openAllWindows() selects every <a> tag on the webpage
+* links.length can be changed to modify the amount
+of popup windows that are opened
+*/
 openAllWindows()
+introWindow()
+introWindow(){
+    let w = 400
+    let h = 60
+    let x = (screen.width/2)-(w/2);
+    let y = (screen.height/2)-(h/2);
+
+    window.open ("1.html", "newWindow1",`menubar=no,status=no,location=no,width=${w},height=${h},toolbar=no,scrollbars=no,top=${y},left=${x}`);
+}
+
 function openAllWindows() {
   let links = document.body.querySelectorAll("a")
   for (let i = 0; i < links.length; i++) {
@@ -21,3 +48,67 @@ function openWindow (url, i){
     width=200,height=150,toolbar=no,scrollbars=no,
     top=${y},left=${x}`);
 }
+/** this is an example
+  of a function I want to run inside of
+  a specific child/(popup) window.
+  The idea being
+  that in each window is selected based on its name
+  and will run the specific window.moveTo() while loop.
+*/
+/**  function injectChild(name){
+
+    function moveWindow(name) {
+      if (name === 'window1') {
+
+        while (true) {
+          window.moveTo(400, 700);
+          window.moveTo(550, 500);
+          window.moveTo(400, 300);
+          window.moveTo(550, 100);
+          window.moveTo(550, 300);
+          window.moveTo(550, 500);
+          window.moveTo(550, 700);
+
+        } else if (name === 'window2') {
+          window.moveTo(100, 400);
+          window.moveTo(550, 500);
+          window.moveTo(400, 300);
+          window.moveTo(550, 100);
+          window.moveTo(550, 300);
+          window.moveTo(550, 500);
+          window.moveTo(550, 700);
+      }
+    }
+  }
+}
+*/
+
+  // selects all '*' generates a random color value
+  // at interval of 100
+  /** function flashing() {
+    setInterval(()=>{
+     document.querySelectorAll('*').forEach(e=>
+       e.style.background='#'+Math.floor(Math.random()*16777215)
+       .toString(16))
+     },100)
+  }
+  */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  /**    AUDIO FUNCTIONS
+
+   if i is greater than or equal to 1, sample
+   will be triggered
+   this still doesnt work ;-()
+
+  function playAudio() {
+    let audio = new Audio('sounds/Morse.aiff')
+      if (i >= 1) {
+        audio.play();
+      } else {
+        console.log("all done")
+      }
+  }
+  */
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
